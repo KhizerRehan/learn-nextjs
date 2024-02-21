dbbcd111e4093bab83684f43b0408664468fdcf4
Khizer Rehan <khizerrehan92@gmail.com>
2024-02-21 17:23:41 +0500

    feat/redux-toolkit: Fix Tailwind classes to align items in center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   const counter = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-center mb-12">
         Count is {counter}
       </div>
 
